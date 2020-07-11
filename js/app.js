904492408dce02af5e097aafde08c55ffab5a9ed
timiodulate <timiodulate@users.noreply.github.com>
2020-07-11 16:17:04 +0100

    added share click event

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
   $("#myTooltips a").tooltip({
     html: true,
-    title: "<p>SHARE<p> <img src='./images/icon-facebook.svg' alt='facebooklogo'><img src='./images/icon-twitter.svg' alt='> <img src='./images/icon-pinterest.svg' alt='pinterestLogo'>",
+    title: "<p>SHARE<p> <img src='./images/icon-facebook.svg' alt='facebooklogo'><img src='./images/icon-twitter.svg' alt=''> <img src='./images/icon-pinterest.svg' alt='pinterestLogo'>",
   });
 });
+
+const shareImage = document.querySelector('.shareImg');
+const shareImageCopy = document.querySelector('.shareImgC');
+const active = document.querySelector('.active');
+
+shareImage.addEventListener('click', () => {
+  active.classList.add('show');
+})
+shareImageCopy.addEventListener('click', () => {
+  active.classList.remove('show');
+})
+// const shareImage = document.querySelector
