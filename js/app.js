e0db6f7412fc014d18886710752e5033b6d714a7
timiodulate <timiodulate@users.noreply.github.com>
2020-08-29 08:50:42 +0100

    adjusted style

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-  $("#myTooltips a").tooltip({
-    html: true,
-    title: "<p>SHARE<p> <img src='./images/icon-facebook.svg' alt='facebooklogo'><img src='./images/icon-twitter.svg' alt=''> <img src='./images/icon-pinterest.svg' alt='pinterestLogo'>",
-  });
+	$("#myTooltips a").tooltip({
+		html: true,
+		title:
+			"<p>SHARE<p> <img src='./images/icon-facebook.svg' alt='facebooklogo'><img src='./images/icon-twitter.svg' alt=''> <img src='./images/icon-pinterest.svg' alt='pinterestLogo'>",
+	});
 });
 
-const shareImage = document.querySelector('.shareImg');
-const shareImageCopy = document.querySelector('.shareImgC');
-const active = document.querySelector('.active');
+const shareImage = document.querySelector(".card__share_image");
+const shareImageCopy = document.querySelector(".card__share_image-c");
+const active = document.querySelector(".card__active");
 
-shareImage.addEventListener('click', () => {
-  active.classList.add('show');
-})
-shareImageCopy.addEventListener('click', () => {
-  active.classList.remove('show');
-})
+shareImage.addEventListener("click", () => {
+	active.classList.add("show");
+});
+shareImageCopy.addEventListener("click", () => {
+	active.classList.remove("show");
+});
 // const shareImage = document.querySelector
